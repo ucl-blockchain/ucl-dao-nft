1debd026c17910c3d760ffd3ed6a4871b0d22a0c
sgno28 <s.ngotuantu@gmail.com>
2022-12-08 13:20:31 +0000

    commit test deploy

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,22 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "erc721a/contracts/ERC721A.sol";
 
-contract NFT is ERC721A {
+contract NFT is ERC721A, Ownable {
+    // TODO: ADD COMMENTS
     uint public immutable maxSupply;
-    constructor(uint _totalSupply) ERC721A("NFT_TESTING_1", "NFT1") {
+    string public baseURI;
+    string public contractURI;
+
+    event ContractURIChanged(
+        address sender,
+        string newContractURI
+    );
+
+    event BaseURIChanged (
+        address sender,
+        string newBaseURI
+    );
+
+    event Airdropped (
+        address recipient,
+        address sender,
+        uint amount
+    );
+
+    constructor(uint _maxSupply, string memory _baseURI, string memory _contractURI) ERC721A("NFT_TESTING_1", "NFT1") {
         maxSupply = _maxSupply;
+        baseURI = _baseURI;
+        contractURI = _contractURI;
     }
-    
-    function airdrop(address[] memory _recipients) external payable {
+
+    function setBaseURI(string calldata _newBaseURI) public onlyOwner{
+        baseURI = _newBaseURI;
+        emit BaseURIChanged(msg.sender, _newBaseURI);
+    }
+
+    function setContractURI(string calldata _newContractURI) public onlyOwner{
+        contractURI = _newContractURI;
+        emit ContractURIChanged(msg.sender, _newContractURI);
+    }
+
+    function airdrop(address[] calldata _recipients) external payable onlyOwner {
         // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
-        uint length = _recipients.length;
-        require(totalSupply() + recipients.length <= maxSupply, "Max supply reached")
-        for (uint i=0; i<length; i++) {
+        require(totalSupply() + _recipients.length <= maxSupply, "Max supply reached");
+        for (uint i=0; i<_recipients.length; i++) {
             address to = _recipients[i];
-            require(to != address(0), "Can't mint to zero address")
+            require(to != address(0), "Can't mint to zero address");
             _safeMint(to, 1);
-        } // To do: add events
+            emit Airdropped(to, msg.sender, 1);
+        }
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,18 +5,21 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const recipients = require("./airdrop_recipients.json")
+
 
 async function main() {
   const NFT = await hre.ethers.getContractFactory("NFT");
-  const nft = await NFT.deploy();
-
+  const baseURI = "https://gateway.pinata.cloud/ipfs/Qmc5YEcgBBUzvDCiUX62eXp92h3B7BaDYHKsFqZZEeEvgV/";
+  const contractURI = "https://gateway.pinata.cloud/ipfs/QmUSLkbvAVNGiMb3rQMUbLGk8X6v7hso1Xj4TEWXM311jH";
+  const nft = await NFT.deploy(3, baseURI, contractURI);
   await nft.deployed();
-
   console.log(
     `nft deployed to ${nft.address}`
   );
+  let txn = await nft.airdrop(recipients);  
+  console.log(txn);
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
